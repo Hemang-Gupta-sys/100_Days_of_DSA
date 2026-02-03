ebb74707469530f3b9a38d7893edfa8b236f35e3
Hemang-Gupta-sys <hemanggupta961@gmail.com>
2026-02-03 22:06:12 +0530

    Day-3_Q-6

---
+++ Day3_Q6.c
@@ -0,0 +1,41 @@
+/*You are given an array arr[] of size n - 1 that contains distinct integers in the range from 1 to n (inclusive). This array represents a permutation of the integers from 1 to n with one element missing. Your task is to identify and return the missing element.
+
+Examples:
+
+Input: arr[] = [1, 2, 3, 5]
+Output: 4
+Explanation: All the numbers from 1 to 5 are present except 4.
+Input: arr[] = [8, 2, 4, 5, 3, 7, 1]
+Output: 6
+Explanation: All the numbers from 1 to 8 are present except 6.
+Input: arr[] = [1]
+Output: 2
+Explanation: Only 1 is present so the missing element is 2.
+Constraints:
+1 ≤ arr.size() ≤ 106
+1 ≤ arr[i] ≤ arr.size() + 1*/
+
+#include <stdio.h>
+#define N 1000000
+int main() {
+    int n, arr[N];
+    printf("Enter the size of the array (n-1): ");
+    scanf("%d", &n);
+
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int expected_sum = (n + 1) * (n + 2) / 2; 
+    int actual_sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        actual_sum += arr[i];
+    }
+
+    int missing_element = expected_sum - actual_sum;
+    printf("The missing element is: %d\n", missing_element);
+
+    return 0;
+}

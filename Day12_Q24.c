f9e080518cd6e54f4759293fe5c1c1a327e47602
Hemang-Gupta-sys <hemanggupta961@gmail.com>
2026-02-12 22:03:38 +0530

    Day-12_Q-24

---
+++ Day12_Q24.c
@@ -0,0 +1,66 @@
+/*Given an m x n matrix, return true if the matrix is Toeplitz. Otherwise, return false.
+
+A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same elements.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+Output: true
+Explanation:
+In the above grid, the diagonals are:
+"[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
+In each diagonal all elements are the same, so the answer is True.
+Example 2:
+
+
+Input: matrix = [[1,2],[2,2]]
+Output: false
+Explanation:
+The diagonal "[1, 2]" has different elements.
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 20
+0 <= matrix[i][j] <= 99*/
+
+#include <stdio.h>
+#include <stdbool.h>
+#define N 100
+bool isToeplitzMatrix(int** matrix, int matrixSize, int* matrixColSize) {
+    for (int i = 0; i < matrixSize - 1; i++) {
+        for (int j = 0; j < matrixColSize[0] - 1; j++) {
+            if (matrix[i][j] != matrix[i + 1][j + 1]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main() {
+    int m, n, matrix[N][N];
+    
+    printf("Enter the number of rows and columns: ");
+    scanf("%d %d", &m, &n);
+    
+    printf("Enter the elements of the matrix:\n");
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    
+    if (isToeplitzMatrix((int**)matrix, m, (int[]){n})) {
+        printf("The matrix is a Toeplitz Matrix\n");
+    } else {
+        printf("The matrix is not a Toeplitz Matrix\n");
+    }
+    
+    return 0;
+}

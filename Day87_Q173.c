a588b130f46e7f5709363537b7b4897deb76566a
Hemang-Gupta-sys <hemanggupta961@gmail.com>
2026-04-28 22:58:13 +0530

    Day-86_Q-173

---
+++ Day87_Q173.c
@@ -0,0 +1,56 @@
+/*Problem: Implement Binary Search Iterative - Implement the algorithm.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the sorted array or search result
+
+Example:
+Input:
+5
+64 34 25 12 22
+
+Output:
+12 22 25 34 64*/
+
+#include <stdio.h>
+
+int binarySearch(int arr[], int n, int target) {
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] < target)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    return -1;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int target;
+    scanf("%d", &target);
+
+    int result = binarySearch(arr, n, target);
+
+    if (result != -1)
+        printf("%d\n", result);
+    else
+        printf("-1\n");
+
+    return 0;
+}
